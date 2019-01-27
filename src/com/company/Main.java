f57be1c44deec5c7a5b34320d1272d1225d5ae51
unknown <mschultz366@students.mchenry.edu>
2019-01-27 08:17:16 -0600

    second attempt

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+                System.out.print(" I'm a fulltime factory worker ");
+                System.out.print(" I am also a fulltime student ");
+                System.out.print(" I hope to major in wildlife biology ");
+                System.out.print("Animals have always been a big part of my life ");
+            }
+        }
